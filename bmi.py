014f1961c3b987a228513039d2f4720a4c22cd67
koize <justinweng6@gmail.com>
2023-10-31 09:41:18 +0800

    returns new value

--- bmi.py
+++ bmi.py
@@ -4,11 +4,14 @@ def calculate_bmi(height,weight):
     bmi = weight / (height * height)
     if bmi < 18.5:
         weightclass = "Under Weight"
+        weight_value = -1
     elif 25 > bmi >= 18.5:
         weightclass = "Normal Weight"
+        weight_value = 0
     else:
         weightclass = "Over Weight"
-    print("BMI = " + str(bmi) + ", " + weightclass)
+        weight_value = 0
+    print("BMI = " + str(bmi) + ", " + weightclass + ", " + str(weight_value))
 
 
 calculate_bmi(weight=57, height=1.73)
